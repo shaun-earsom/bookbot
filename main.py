46518b896f70e6118dbfb1c1e1678a85d47f43aa
shaun-earsom <searsom@icloud.com>
2024-03-08 07:17:06 -0800

    optimized count_letters

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
+    num_letters = count_letters(text)
     print(f"{num_words} words found in the document")
+    print(f"Letter Report: {num_letters}")
 
 
 def get_book_text(path):
@@ -12,5 +14,15 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    letters = {}
+    for letter in text:
+        l_letter = letter.lower()
+        if l_letter in letters:
+            letters[l_letter] += 1
+        else:
+            letters[l_letter] = 1
+    return letters
 
 main()
